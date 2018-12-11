f7ea2b147f826f12b0561fe9848ab58e6556f8e6
Sem Bogaarts <sembogaarts@gmail.com>
2018-12-11 14:35:21 +0100

    Coords update

--- src/main/java/com/richrail/CloneTest.java
+++ src/main/java/com/richrail/CloneTest.java
@@ -44,34 +44,44 @@ public class CloneTest extends Application {
 
         Group g = new Group();
 
+        // Properties
+        int bodyHeight = 60;
+        int bodyWidth = 120;
+        int pipeHeight = 90;
+        int pipeWidth = 50;
+        int wheelRadius = 15;
+        int wheelOffset = 10;
+
         // Create the body
         Rectangle body = new Rectangle();
-        body.setWidth(120);
-        body.setHeight(60);
+        body.setWidth(bodyWidth);
+        body.setHeight(bodyHeight);
         body.setX(x);
-        body.setY(y);
+        body.setY(y - (bodyHeight - pipeHeight));
         body.setFill(Color.GREY);
 
         // Create the pipe
         Rectangle pipe = new Rectangle();
-        pipe.setWidth(50);
-        pipe.setHeight(90);
-        pipe.setX(x + 70);
-        pipe.setY(y - 40);
+        pipe.setWidth(pipeWidth);
+        pipe.setHeight(pipeHeight);
+
+        pipe.setX(x + (bodyWidth - pipeWidth));
+        pipe.setY(y);
+
         pipe.setFill(Color.GREY);
 
         // Create the wheel
         Circle frontWheel = new Circle();
-        frontWheel.setRadius(15);
-        frontWheel.setCenterX(x + 25);
-        frontWheel.setCenterY(y + 75);
+        frontWheel.setRadius(wheelRadius);
+        frontWheel.setCenterX(x + (wheelRadius + wheelOffset));
+        frontWheel.setCenterY(y + pipeHeight);
         frontWheel.setFill(Color.BLACK);
 
         // Create the wheel
         Circle backWheel = new Circle();
-        backWheel.setRadius(15);
-        backWheel.setCenterX(x + 85);
-        backWheel.setCenterY(y + 75);
+        backWheel.setRadius(wheelRadius);
+        backWheel.setCenterX(x + (bodyWidth - wheelRadius - wheelOffset));
+        backWheel.setCenterY(y + pipeHeight);
         backWheel.setFill(Color.BLACK);
 
         // Add all to group
@@ -89,26 +99,34 @@ public class CloneTest extends Application {
 
         Group g = new Group();
 
+        // Properties
+        int bodyHeight = 60;
+        int bodyWidth = 120;
+        int pipeHeight = 90;
+        int pipeWidth = 50;
+        int wheelRadius = 15;
+        int wheelOffset = 10;
+
         // Create the body
         Rectangle body = new Rectangle();
-        body.setWidth(120);
-        body.setHeight(60);
+        body.setWidth(bodyWidth);
+        body.setHeight(bodyHeight);
         body.setX(x);
-        body.setY(y);
+        body.setY(y - (bodyHeight - pipeHeight));
         body.setFill(Color.GREY);
 
         // Create the wheel
         Circle frontWheel = new Circle();
-        frontWheel.setRadius(15);
-        frontWheel.setCenterX(x + 25);
-        frontWheel.setCenterY(y + 75);
+        frontWheel.setRadius(wheelRadius);
+        frontWheel.setCenterX(x + (wheelRadius + wheelOffset));
+        frontWheel.setCenterY(y + pipeHeight);
         frontWheel.setFill(Color.BLACK);
 
         // Create the wheel
         Circle backWheel = new Circle();
-        backWheel.setRadius(15);
-        backWheel.setCenterX(x + 85);
-        backWheel.setCenterY(y + 75);
+        backWheel.setRadius(wheelRadius);
+        backWheel.setCenterX(x + (bodyWidth - wheelRadius - wheelOffset));
+        backWheel.setCenterY(y + pipeHeight);
         backWheel.setFill(Color.BLACK);
 
         // Add all to group
